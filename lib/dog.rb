ffcc7bbf23b2fb95e276d01f8bbf282a86cc6c30
Kevin Downs <kevin.downs04@gmail.com>
2019-12-17 19:31:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ class Dog
     dog = DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
   end
   
-  def self.find_or_create_by
+  def self.find_or_create_by(hash)
     
   end
   
